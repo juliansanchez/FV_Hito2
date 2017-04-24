2e488c1f30f3339031f19e8826803688e0b38413
juliansanchez <eljovenjuli@hotmail.com>
2017-04-24 12:42:26 +0200

    master

--- Hito2-Personaje/nbproject/Package-Debug.bash
+++ Hito2-Personaje/nbproject/Package-Debug.bash
@@ -13,9 +13,9 @@ CND_BUILDDIR=build
 CND_DLIB_EXT=so
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/texturapersonaje
-OUTPUT_BASENAME=texturapersonaje
-PACKAGE_TOP_DIR=texturapersonaje/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/hito2-personaje
+OUTPUT_BASENAME=hito2-personaje
+PACKAGE_TOP_DIR=hito2-personaje/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/texturapersonaje/bin"
+makeDirectory "${NBTMPDIR}/hito2-personaje/bin"
 copyFileToTmpDir "${OUTPUT_PATH}" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/texturapersonaje.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/hito2-personaje.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/texturapersonaje.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/hito2-personaje.tar *
 checkReturnCode
 
 # Cleanup
